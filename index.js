b5278f1a30066baff732a82930b74021e641cb06
hai-nguyen1112 <hainguyen871112@gmail.com>
2020-01-10 12:01:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(array) {
+  let min
+  let index
+  for (let i = 0; i < array.length; i++) {
+    if (i === 0) {
+      min = array[i]
+    } else {
+      if (array[i] < min) {
+        min = array[i]
+        index = i
+      }
+    }
+  }
+  array.splice(index, 1)
+  return min
 }
 
-function insertionSort(array){
-
+function insertionSort(array) {
+  let sortedArray = []
+  while (array.length !== 0) {
+    sortedArray.push(findMinAndRemove(array))
+  }
+  return sortedArray
 }
